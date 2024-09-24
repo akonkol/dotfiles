357e32b8e366b4b4b80cce0c49b487bb88bd51f5
Andrew Konkol <andrewkonkol@fullstory.com>
2024-09-24 16:04:26 -0500

    add gbrowse

--- .config/nvim/lua/akonkol/maps.lua
+++ .config/nvim/lua/akonkol/maps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>nt", function () vim.cmd.Neotree("toggle") end)
 vim.keymap.set("n", "<leader>qq", function () vim.cmd("qa") end)
 vim.keymap.set({"n", "v"}, "<leader>cc", "gc", {remap = true})
+vim.keymap.set({"n", "v"}, "<leader>b", function () vim.cmd("GBrowse") end)
