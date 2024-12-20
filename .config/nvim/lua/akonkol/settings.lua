70e8b7c3a8ba4e8066bb77790f0774c4881e54a1
Andrew Konkol <andrewkonkol@fullstory.com>
2024-12-20 12:02:26 -0600

    add format per lsp

--- .config/nvim/lua/akonkol/settings.lua
+++ .config/nvim/lua/akonkol/settings.lua
@@ -13,12 +13,19 @@ o.scrolloff = 12
 o.undofile = true
 o.undoreload = 10000
 o.cmdheight = 0
--- Run GoFmt when saving .go files
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimports()
-  end,
-  group = format_sync_grp,
+
+-- 1
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    -- 2
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      -- 3
+      buffer = args.buf,
+      callback = function()
+        -- 4 + 5
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
 })
