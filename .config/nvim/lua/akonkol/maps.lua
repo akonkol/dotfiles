7270f357a4a1a8494b00892f5146f2c4a79b810b
Andrew Konkol <825835+akonkol@users.noreply.github.com>
2024-12-26 19:08:47 -0600

    leader ee does a err block

--- .config/nvim/lua/akonkol/maps.lua
+++ .config/nvim/lua/akonkol/maps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>nt", function () vim.cmd.Neotree("toggle","current") end)
 vim.keymap.set("n", "<leader>qq", function () vim.cmd("qa") end)
 vim.keymap.set({"n", "v"}, "<leader>cc", "gcc", {remap = true})
+vim.keymap.set("n", "<leader>ee", function () vim.cmd("r~/.config/nvim/templates/golang-errblock.txt") end)
